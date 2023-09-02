d2527c98009c21a93a7b39c3a6ed45cccdf53058
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-09-02 09:35:43 +0400

    :tada: Sets up scaffold core

---
+++ packages/reactivity/src/index.ts
@@ -0,0 +1,7 @@
+export const Signal = () => {
+  throw new Error('Signal not implemented');
+};
+
+export const Effect = () => {
+  throw new Error('Effect not implemented');
+};
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  root: resolve(__dirname),
+  plugins: [tsconfigPaths()],
+  build: {
+    target: 'esnext',
+  },
+  test: {
+    globals: true,
+    include: ['./**/*{.spec,.test}.{ts,tsx}'],
+    includeSource: ['./**/*.{ts,tsx}'],
+    reporters: ['dot'],
+    deps: {},
+    useAtomics: true,
+    passWithNoTests: true,
+  },
+});
